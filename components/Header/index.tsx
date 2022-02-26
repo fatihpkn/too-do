c26b34760c082f1b3b0c8ea23d649ed3ed36f0ef
fatihpkn <pknfatih@gmail.com>
2022-02-26 18:30:44 +0300

    :lipstick: :art: Tailwind added

---
+++ components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IHeaderProps {
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+  return <div></div>;
+};
+
+export default Header;
